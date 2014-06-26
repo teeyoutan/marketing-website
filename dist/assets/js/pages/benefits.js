988ffc990aa7208c7d2c444e0b7a8cbad2399b80
Andy Creighton <acreigh2@gmail.com>
2014-06-26 17:20:38 -0500

    re-minified js

--- dist/assets/js/pages/benefits.js
+++ dist/assets/js/pages/benefits.js
@@ -1,15 +1,1 @@
-(function($) {
-    window.optly = window.optly || {};
-    window.optly.mrkt = window.optly.mrkt || {};
-    window.optly.mrkt.benefits = {};
-    window.optly.mrkt.benefits.menu = function() {
-        $("body").delegate(".for-menu", "click", function(event) {
-            event.stopPropagation();
-            $(this).toggleClass("shown");
-        });
-        $("html").click(function() {
-            $(".for-menu").removeClass("shown");
-        });
-    };
-    window.optly.mrkt.benefits.menu();
-})(jQuery);
+(function($){window.optly=window.optly||{};window.optly.mrkt=window.optly.mrkt||{};window.optly.mrkt.benefits={};window.optly.mrkt.benefits.menu=function(){$("body").delegate(".for-menu","click",function(event){event.stopPropagation();$(this).toggleClass("shown")});$("html").click(function(){$(".for-menu").removeClass("shown")})};window.optly.mrkt.benefits.menu()})(jQuery);
