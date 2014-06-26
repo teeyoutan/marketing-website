c773d03e7528ca2c76a843be9e512ce91b266f53
dtothefp <dtothefp@gmail.com>
2014-06-25 23:36:25 -0700

    Responsive grid of equal heights, mixins for responsive video, custom helpers for grid row class upon iteration, modifications of jshint for dev environment

--- helpers/helper-classgridstart.js
+++ helpers/helper-classgridstart.js
@@ -1,11 +1,10 @@
 // module.exports = require('./lib/helper-lib');
 
 module.exports.register = function (Handlebars, options, params)  { 
-  Handlebars.registerHelper('classgrid', function (index, options)  { 
-    gridclass: function (index, options) {
-    if (index === 0 || index % 4 === 0) {
+  Handlebars.registerHelper('classgridstart', function (index, options)  { 
+    if (index === 0 || index % 3 === 0) {
         return options.fn(this);
-      }
+    }
     return options.inverse(this);
-  };
+  });
 };
