b09ec02ee45522eb49e99e34ee142034ed86e611
Andy Creighton <acreigh2@gmail.com>
2014-07-15 14:06:48 -0500

    initialize slider

--- website-guts/assets/js/pages/getting-started.js
+++ website-guts/assets/js/pages/getting-started.js
@@ -1,15 +1,20 @@
 window.optly.mrkt.gettingStarted = {};
 
+window.optly.mrkt.gettingStarted.init = function() {
+    $('.controls').find('a[data-slide=one]').addClass('active');
+};
+
 window.optly.mrkt.gettingStarted.changeSlide = function(input) {
     $('.slider ul').attr('class', '').addClass(input);
     $('.controls a').removeClass('active').parent().find('[data-slide="' + input + '"]').addClass('active');
 };
 
-window.optly.mrkt.gettingStarted.init = function(event) {
+window.optly.mrkt.gettingStarted.handleClick = function(event) {
     event.preventDefault();
     
     var nextSlide = $(this).attr('data-slide');
     window.optly.mrkt.gettingStarted.changeSlide(nextSlide);
 };
 
-$('[data-slide]').click(window.optly.mrkt.gettingStarted.init);
+window.optly.mrkt.gettingStarted.init();
+$('[data-slide]').click(window.optly.mrkt.gettingStarted.handleClick);
