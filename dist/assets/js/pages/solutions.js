967c59f42bbc731e3df7a69612ae4632a50f29d4
Jared Laser <laserjn@gmail.com>
2014-08-05 16:38:46 -0400

    Filter items inclusively using isotope

--- dist/assets/js/pages/solutions.js
+++ dist/assets/js/pages/solutions.js
@@ -4,26 +4,58 @@
 
   window.optly.mrkt = window.optly.mrkt || {}; 
 
-window.optly = window.optly || {};
-window.optly.mrkt = window.optly.mrkt || {};
-window.optly.mrkt.filter = {};
 var $dropdownElems = $('.dropdown-cont');
+var $filterElems = $('.filter-item');
+var $isoContainer;
 
 // FUNCTIONS
 window.optly.mrkt.filter = {
 
   binder: function() {
 
-    $dropdownElems.on('click', function() {
-      var $self = $(this);
-      $dropdownElems.not( $self ).removeClass( 'active' );
-      $self.toggleClass( 'active' );
+    var self = this;
+
+    $dropdownElems.bind('click', function() {
+      var $this = $(this);
+      $dropdownElems.not( $this ).removeClass( 'active' );
+      $this.toggleClass( 'active' );
     });
+
+    $filterElems.bind('click', function(e) {
+      e.stopPropagation();
+      var $this = $(this);
+      $this.toggleClass( 'active' );
+      self.updateIsotope();
+    });
+
+  },
+
+  isotope: function() {
+    $isoContainer = $('.partner-grid').isotope({
+      itemSelector: '.partner-grid-elm',
+      layoutMode: 'fitRows'
+    });
+  },
+
+  updateIsotope: function() {
+    var $output = $('#output');
+    var $activeItems = $filterElems.filter('.active');
+    var values = [];
+
+    $activeItems.each( function() {
+      var value = $(this).data( 'filter' );
+      values.push( '.' + value );
+    });
+
+    var filterValue = values.join(', ');
+    $output.text( filterValue );
+    $isoContainer.isotope({ filter: filterValue });
 
   },
 
   init: function() {
     this.binder();
+    this.isotope();
   }
 
 };
