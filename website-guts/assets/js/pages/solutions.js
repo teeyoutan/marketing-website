7e166a2c8c91aa40111b4258b6593195b4e01e5e
Jared Laser <laserjn@gmail.com>
2014-08-07 12:15:22 -0400

    equalize heights on solutions page

--- website-guts/assets/js/pages/solutions.js
+++ website-guts/assets/js/pages/solutions.js
@@ -25,6 +25,16 @@ window.optly.mrkt.filter = {
   },
 
   isotope: function() {
+
+    var heights = [];
+    $('.partner-grid-elm').each( function() {
+      heights.push( $(this).outerHeight() );
+    });
+    heights = heights.sort().reverse();
+    $('.partner-grid-elm').each( function() {
+      $(this).height( heights[0] );
+    });
+
     $isoContainer = $('.partner-grid').isotope({
       itemSelector: '.partner-grid-elm',
       layoutMode: 'fitRows'
