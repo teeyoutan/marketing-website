e78891b701d0c3bbc0f66964daa485d56fa22a67
dtothefp <dtothefp@gmail.com>
2014-08-07 17:18:38 -0400

    add cursor: pointer to utility nav and remove alerts from signin

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -9039,7 +9039,7 @@ var $signinModal = $('[data-optly-modal="signin"]');
 
 function requestSignin(e) {
   e.preventDefault();
-  window.alert('singin fired');
+
   var deferred = window.optly.mrkt.services.xhr.makeRequest({
     type: 'POST',
     url: '/account/signin'
@@ -9048,14 +9048,12 @@ function requestSignin(e) {
 
   deferred.then(function(data) {
     if (data.success === 'true') {
-      window.alert('response data: ' + data.success);
       if(sessionStorage !== undefined) {
         sessionStorage.modalType = '';
       }
       window.location = 'https://www.optimizely.com/dashboard';
     }
-  }, function(err) {
-    window.alert('response error: ', err);  
+  }, function(err) {  
     console.log('singin error: ' + err);
   });
 
--- website-guts/assets/js/components/modal-signin.js
+++ website-guts/assets/js/components/modal-signin.js
@@ -2,7 +2,7 @@ var $signinModal = $('[data-optly-modal="signin"]');
 
 function requestSignin(e) {
   e.preventDefault();
-  window.alert('singin fired');
+
   var deferred = window.optly.mrkt.services.xhr.makeRequest({
     type: 'POST',
     url: '/account/signin'
@@ -11,14 +11,12 @@ function requestSignin(e) {
 
   deferred.then(function(data) {
     if (data.success === 'true') {
-      window.alert('response data: ' + data.success);
       if(sessionStorage !== undefined) {
         sessionStorage.modalType = '';
       }
       window.location = 'https://www.optimizely.com/dashboard';
     }
-  }, function(err) {
-    window.alert('response error: ', err);  
+  }, function(err) {  
     console.log('singin error: ' + err);
   });
 
