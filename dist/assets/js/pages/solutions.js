4f746a9c1dc63f82bc9b112d9b0a79b561c661b5
Jared Laser <laserjn@gmail.com>
2014-08-18 10:29:50 -0400

    update tooltip to have more info

--- dist/assets/js/pages/solutions.js
+++ dist/assets/js/pages/solutions.js
@@ -15,22 +15,47 @@ window.optly.mrkt.filter = {
 
     var self = this;
 
-    $dropdownElems.bind('click', function() {
+    $dropdownElems.hover(function(e) {
+      e.stopPropagation();
       var $this = $(this);
       $dropdownElems.not( $this ).removeClass( 'active' );
       $this.toggleClass( 'active' );
+
+      // $(window).not( $dropdownElems ).on('click', function() {
+      //   $dropdownElems.removeClass( 'active' );
+      // });
     });
 
     $filterElems.bind('click', function(e) {
       e.stopPropagation();
       var $this = $(this);
+      $this.parent().find('li').not( $this ).removeClass( 'active' );
       $this.toggleClass( 'active' );
+      self.updateIsotope();
+    });
+
+    $('.filter--reset').on('click', function(e) {
+      e.preventDefault();
+      $filterElems.removeClass( 'active' );
       self.updateIsotope();
     });
 
   },
 
   isotope: function() {
+
+    var heights = [];
+    $('.partner-grid-elm').each( function() {
+      heights.push( $(this).outerHeight() );
+    });
+
+    heights = heights.sort().reverse();
+    $('.partner-grid-elm').each( function() {
+      $(this).height( heights[0] );
+    });
+
+    $('.integrations-container').css('min-height', heights[0]);
+
     $isoContainer = $('.partner-grid').isotope({
       itemSelector: '.partner-grid-elm',
       layoutMode: 'fitRows'
@@ -38,18 +63,26 @@ window.optly.mrkt.filter = {
   },
 
   updateIsotope: function() {
-    var $output = $('#output');
     var $activeItems = $filterElems.filter('.active');
     var values = [];
 
     $activeItems.each( function() {
-      var value = $(this).data( 'filter' );
+      var value = $(this).data( 'filter' ).trim();
       values.push( '.' + value );
     });
 
-    var filterValue = values.join(', ');
-    $output.text( filterValue );
+    var filterValue = values.join('');
     $isoContainer.isotope({ filter: filterValue });
+
+    // create a div#output to enable classname debugging
+    // var $output = $('#output');
+    // $output.text( filterValue );
+
+    if ( !$isoContainer.data('isotope').filteredItems.length ) {
+      $('.integrations-message').addClass('visible');
+    } else {
+      $('.integrations-message').removeClass('visible');
+    }
 
   },
 
