fbc7208f5d4a198dbb96a86fe6c7de2419d9fff5
Kyle Rush <kylerrush@gmail.com>
2014-08-28 17:08:08 -0400

    Fix staging deploy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -429,15 +429,21 @@ module.exports = function(grunt) {
         access: 'public-read'
       },
       staging: {
-          upload: [
-            {
-              src: '<%= config.dist %>/**/*',
-              dest: '<%= grunt.option("branch") || gitinfo.local.branch.current.name %>',
-              rel: '<%= config.dist %>'
-            }
-          ]
+        options: {
+          bucket: '<%= grunt.config.get("aws.staging_bucket") %>'
+        },
+        upload: [
+          {
+            src: '<%= config.dist %>/**/*',
+            dest: '<%= grunt.option("branch") || gitinfo.local.branch.current.name %>',
+            rel: '<%= config.dist %>'
+          }
+        ]
       },
       smartling: {
+        options: {
+          bucket: '<%= grunt.config.get("aws.smartling_staging_bucket") %>'
+        },
         upload: [
           {
             src: '<%= config.dist %>/**/*',
