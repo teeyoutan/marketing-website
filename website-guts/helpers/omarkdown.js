16673f7fd0e6c46c41639abcecd077cfda31eac8
Kyle Rush <kylerrush@gmail.com>
2014-08-28 16:50:56 -0400

    Smartling fixes.

--- website-guts/helpers/omarkdown.js
+++ website-guts/helpers/omarkdown.js
@@ -3,7 +3,8 @@ module.exports.register = function (Handlebars)  {
   Handlebars.registerHelper('omarkdown', function (options)  {
     debugger;
     marked.setOptions({
-      linkPath: options.hash.path
+      linkPath: options.hash.path,
+      smartypants: true
     });
     return marked(options.fn(this));
   });
